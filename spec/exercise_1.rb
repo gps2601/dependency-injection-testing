34075832c78d95a88f622033ec6d48d0865fcf4b
Geoff Smith <smith260194@gmail.com>
2019-05-17 07:00:56 +0100

    Before exercise 1

---
+++ spec/exercise_1.rb
@@ -0,0 +1,21 @@
+# Amend the following to be testable in isolation.
+
+class Note
+  def initialize(title, body)
+    @title = title
+    @body = body
+    @formatter = NoteFormatter.new
+  end
+
+  def display
+    @formatter.format(self)
+  end
+
+  attr_reader :title, :body
+end
+
+class NoteFormatter
+  def format(note)
+    "Title: #{note.title}\n#{note.body}"
+  end
+end
